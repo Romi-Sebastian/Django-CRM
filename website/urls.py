8781b12ad39f9c4b9b11ed54a88e8fc40610298d
Romi Sebastian <romisebastian@gmail.com>
2025-04-21 13:47:16 -0400

    logout logic

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('login/', views.login_user, name='login'),
+    # path('login/', views.login_user, name='login'), Use this for a separate login page + view
     path('logout/', views.logout_user, name='logout'),
 
 ]
--- website/views.py
+++ website/views.py
@@ -23,5 +23,8 @@ def home(request):
 
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, 'You Have Been Logged Out...')
+    return redirect('home')
+
 
