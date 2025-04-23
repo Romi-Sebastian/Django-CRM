8188c88a289a56229d5c1dc51d0514a29b98d14b
Romi Sebastian <romisebastian@gmail.com>
2025-04-23 01:04:33 -0400

    Added a user profile

--- website/urls.py
+++ website/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('delete_record/<int:pk>', views.delete_record, name='delete'),
     path('add_record/', views.add_record, name='add'),
     path('update_record/<int:pk>', views.update_record, name='update'),
+    path('profile/', views.profile_view, name='profile')
 
 ]
