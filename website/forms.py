e190863472a70306a5d0626bb073aa75384a5999
Romi Sebastian <romisebastian@gmail.com>
2025-04-22 23:39:14 -0400

    Added the functionality to update a Record

--- website/forms.py
+++ website/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
 
 
 class SignUpForm(UserCreationForm):
@@ -40,3 +41,24 @@ class SignUpForm(UserCreationForm):
                                              'before, for verification.</small></span>'
 
 
+class AddRecordForm(forms.ModelForm):
+    first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(
+        attrs={'placeholder': 'First Name', 'class': 'form-control'}), label='')
+    last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(
+        attrs={'placeholder': 'Last Name', 'class': 'form-control'}), label='')
+    email = forms.CharField(required=True, widget=forms.widgets.TextInput(
+        attrs={'placeholder': 'Email', 'class': 'form-control'}), label='')
+    phone = forms.CharField(required=True, widget=forms.widgets.TextInput(
+        attrs={'placeholder': 'phone', 'class': 'form-control'}), label='')
+    address = forms.CharField(required=True, widget=forms.widgets.TextInput(
+        attrs={'placeholder': 'address', 'class': 'form-control'}), label='')
+    city = forms.CharField(required=True, widget=forms.widgets.TextInput(
+        attrs={'placeholder': 'city', 'class': 'form-control'}), label='')
+    state = forms.CharField(required=True, widget=forms.widgets.TextInput(
+        attrs={'placeholder': 'State', 'class': 'form-control'}), label='')
+    zipcode = forms.CharField(required=True, widget=forms.widgets.TextInput(
+        attrs={'placeholder': 'Zipcode', 'class': 'form-control'}), label='')
+
+    class Meta:
+        model = Record
+        exclude = ('user',)
